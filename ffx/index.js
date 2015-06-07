54189a7f01bccd7d349f22745354ab4e3d500387
Hiroyuki Sano <sh19910711@gmail.com>
2015-06-07 19:21:32 +0900

    ffx: test tabs APIs

--- ffx/index.js
+++ ffx/index.js
@@ -6,4 +6,16 @@ function dummy(text, callback) {
   callback(text);
 }
 
+// Test tabs APIs
+var tabs = require('sdk/tabs');
+var tabUtils = require('sdk/tabs/utils');
+var viewCore = require('sdk/view/core')
+var viewFor = viewCore.viewFor;
+function getBrowserTest(tab) {
+  var lowLevelTab = viewFor(tab);
+  var browser = tabUtils.getBrowserForTab(lowLevelTab);
+  console.log(browser);
+}
+tabs.on("ready", getBrowserTest);
+
 exports.dummy = dummy;
